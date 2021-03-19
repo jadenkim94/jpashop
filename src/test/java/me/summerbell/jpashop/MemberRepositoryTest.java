9e9c21c134a5bce798008a9d8bf6315e82b1f4e5
jaden <jonghaha7@gmail.com>
2021-03-19 15:56:23 +0900

    Initial commit

---
+++ src/test/java/me/summerbell/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,33 @@
+package me.summerbell.jpashop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+    
+    @Test
+    @Transactional
+    @Rollback(false)
+    void testMember() throws Exception {
+    //given
+        Member member = new Member();
+        member.setUsername("jaden");
+    //when
+        Long savedId = memberRepository.save(member);
+        Member findMember = memberRepository.find(savedId);
+    //then
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member);
+    }
+
+}
