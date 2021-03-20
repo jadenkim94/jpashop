63a0cef50b976889c6229342629799129f22eb90
jaden <jonghaha7@gmail.com>
2021-03-21 01:20:03 +0900

    :saprkles: 상품  등록, 상품  조회 구현 및 테스트

--- src/main/java/me/summerbell/jpashop/domain/item/Item.java
+++ src/main/java/me/summerbell/jpashop/domain/item/Item.java
@@ -3,6 +3,7 @@ package me.summerbell.jpashop.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import me.summerbell.jpashop.domain.Category;
+import me.summerbell.jpashop.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -27,6 +28,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //== 비즈니스 로직==//
 
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+    /**
+     *  stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 
 }
